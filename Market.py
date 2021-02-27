e6348d0cbb8cfff58d8dcb955736d7a9d4901fff
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-02-27 12:39:47 +0000

    Renamed market.py to Market.py

---
+++ Market.py
@@ -0,0 +1,14 @@
+class Market(object):
+    def __init__(self, assets):
+        Market.assets = assets
+        Market.efficient_frontier
+        
+
+
+
+    #Getters
+    def getAssets(self):
+        return Market.assets
+
+    def getCovarianceMatrix(self):
+        return
