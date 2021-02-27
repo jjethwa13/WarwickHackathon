ce365765114347cb5a018bc5a1e2542cab8d97b5
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-02-27 14:18:10 +0000

    Added setCovarianceMatrix function

--- market.py
+++ market.py
@@ -1,17 +1,29 @@
-class Market(object):
+import numpy as np
+class Market():
     def __init__(self, assets):
         Market.assets = assets
         Market.efficient_frontier
-        Market.covariance_matrix
+        Market.covariance_matrix = np.diag(np.ones(len(self.assets)))
         Market.riskless_asset
 
     '''Getters'''
-    def getAssets(self):
-        return Market.assets
+    def get_assets(self):
+        return self.assets
+
     def getCovarianceMatrix(self):
-        return Market.getCovarianceMatrix
+        return self.getCovarianceMatrix
 
     '''Create riskless asset'''
     def createRisklessAsset(self):
-        Market.riskless_asset = Asset()
-        return Market.riskless_asset
+        self.riskless_asset = Asset(rf=0.5)
+        return self.riskless_asset
+
+    '''Creates covariance matrix for the assets in the market'''
+    def setCovarianceMatrix(self):
+        def makeSymmetric(arr):
+            return arr + arr.T - np.diag(arr.diagonal())
+
+        for i in range(len(self.assets)):
+            for j in range(i):
+                self.covariance[j][i]=self.assets[i].getCovariance(self.assets[j])
+        return self.covariance_matrix
