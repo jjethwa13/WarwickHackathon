e53b8b23357e05726e91e90e444e7513a0aedfe2
Jay Jethwa <54906219+jjethwa13@users.noreply.github.com>
2021-02-27 19:42:29 +0000

    Fixes 2.0

--- Test.py
+++ Test.py
@@ -3,24 +3,20 @@ import Market
 import Asset
 import matplotlib.pyplot as plt
 
+from scipy.stats import norm
 
-
-    
-
+def TestDist(x):
+    return norm.cdf(x)
     
 X = Asset.Asset()
 X.setDistribution(TestDist)
 
 Y = Asset.Asset()
-Y.setDistribution(TestDist2)
+Y.setDistribution(TestDist)
 
-#S = Market.Market([X,Y], 0.01)
-#
-#print(S.getCovarianceMatrix())
+S = Market.Market([X,Y], 0.01)
 
-print(X.simulate(n=10000))
-
-#print(X.getCovar(Y))
+print(S.getCovarianceMatrix())
 
 
 
