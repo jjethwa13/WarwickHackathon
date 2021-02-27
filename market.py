4dbb8777f40bb80ca7b3a543929927dbc7e89d7c
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-02-27 12:44:11 +0000

    Added return values to getters

--- market.py
+++ market.py
@@ -2,9 +2,10 @@ class Market(object):
     def __init__(self, assets):
         Market.assets = assets
         Market.efficient_frontier
-        
+        Market.covariance_matrix
+
     '''Getters'''
     def getAssets(self):
         return Market.assets
     def getCovarianceMatrix(self):
-        return
+        return Market.getCovarianceMatrix
