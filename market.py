233480bd9708acd4b1baa78ef7981719811dd7e6
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-02-27 13:13:17 +0000

    Added createRisklessAsset function

--- market.py
+++ market.py
@@ -3,9 +3,15 @@ class Market(object):
         Market.assets = assets
         Market.efficient_frontier
         Market.covariance_matrix
+        Market.riskless_asset
 
     '''Getters'''
     def getAssets(self):
         return Market.assets
     def getCovarianceMatrix(self):
         return Market.getCovarianceMatrix
+
+    '''Create riskless asset'''
+    def createRisklessAsset(self):
+        Market.riskless_asset = Asset()
+        return Market.riskless_asset
