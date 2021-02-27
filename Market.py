5b97494a724d5f4387c84d8d4df49d2fa2fcdeda
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-02-27 15:57:57 +0000

    Created tangency and min var portfolio

--- Market.py
+++ Market.py
@@ -1,11 +1,18 @@
 import numpy as np
 class Market():
-    def __init__(self, assets):
-        Market.assets = assets
-        Market.efficient_frontier
-        Market.covariance_matrix = np.diag(np.ones(len(self.assets)))
-        Market.riskless_asset
-        Market.expected_returns = [asset.getExpectation() for asset in assets]    
+    def __init__(self, assets, rf):
+        self.assets = assets
+        self.efficient_frontier
+
+        self.covariance_matrix = np.diag(np.ones(len(self.assets)))
+        setCovarianceMatrix()
+        self.inv_covariance_matrix
+        setInvCovarianceMatrix()
+
+        self.riskless_asset
+        self.expected_returns = [asset.getExpectation() for asset in assets]
+        self.tangency_portfolio
+        self.rf = rf
 
     '''
     Getters
@@ -26,8 +33,7 @@ class Market():
     '''
     Creates covariance matrix for the assets in the market
     '''
-    def setCovarianceMatrix(self):
-        
+    def setCovarianceMatrix(self):   
         def makeSymmetric(arr):
             return arr + arr.T - np.diag(arr.diagonal())
 
@@ -35,5 +41,26 @@ class Market():
             for j in range(i):
                 self.covariance[j][i]=self.assets[i].getCovariance(self.assets[j])
         return self.covariance_matrix
+    
+    def setInvCovarianceMatrix(self):
+        return self.inv_covariance_matrix = np.linalg.inv(self.covariance_matrix)
 
-        
+    def minVariancePortfolio(self, mean):
+        '''
+        Calculates the minimum variance portfolio
+        '''
+        a = self.expected_returns-self.rf*np.ones(len(self.expected_returns))
+        b = np.matmul(inv_covariance_matrix,a)
+        min_variance_portfolio = ((mean-self.rf)*b)/np.matmul(a,b)
+        return min_variance_portfolio
+
+    def tangencyPortfolio(self):
+        '''
+        Calculates the markowitz tangency porfolio
+        '''
+        ones = np.ones(len(self.assets))
+        A = np.matmul(ones,np.matmul(self.inv_covariance_portfolio,ones))
+        B = np.matmul(ones,np.matmul(self.inv_covariance_portfolio,self.expected_returns))
+        a = np.matmul(inv_covariance_matrix,self.expected_returns-self.rf*np.ones(len(self.expected_returns)))
+        self.tangency_portfolio = a/(B-r*A)
+        return self.tangency_portfolio
